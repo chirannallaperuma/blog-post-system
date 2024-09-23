8cc4e1eba1d1bd02ed76e60e8096ec6e06cdad3d
chiran nallaperuma <chiran@intaap.com>
2024-09-23 16:26:59 +0530

    posts create, update, delete done

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -6,9 +6,7 @@
             <div class="col-md-10 mt-5">
                 <div class="card">
                     <div class="card-header d-flex justify-content-between align-items-center">
-                        <div class="card-title">
-                            New Blog Post
-                        </div>
+                        <div class="card-title">New Blog Post</div>
                         <a href="{{ route('blogs.list') }}">
                             <button type="button" class="btn btn-success btn-sm">
                                 <i class="fa fa-list"></i> Blog Post List
@@ -23,14 +21,22 @@
                         </div>
                     @endif
 
+                    @if (session('error'))
+                        <div class="alert alert-danger">
+                            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                            {{ session('error') }}
+                        </div>
+                    @endif
+
                     <div class="card-body">
                         <form action="{{ route('blogs.store') }}" method="POST">
                             @csrf
                             <div class="form-group row mb-3">
                                 <label for="title" class="col-sm-2 col-form-label">Title</label>
                                 <div class="col-sm-10">
-                                    <input type="text" name="title" class="form-control" id="title"
-                                        placeholder="title">
+                                    <input type="text" name="title"
+                                        class="form-control @error('title') is-invalid @enderror" id="title"
+                                        placeholder="title" value="{{ old('title') }}">
                                     @error('title')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -41,7 +47,8 @@
                             <div class="form-group row mb-3">
                                 <label for="description" class="col-sm-2 col-form-label">Description</label>
                                 <div class="col-sm-10">
-                                    <textarea type="text" name="description" class="form-control" id="description" placeholder="description"></textarea>
+                                    <textarea name="description" class="form-control @error('description') is-invalid @enderror" id="description"
+                                        placeholder="description">{{ old('description') }}</textarea>
                                     @error('description')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
@@ -51,16 +58,17 @@
                             </div>
                             <div class="form-group row">
                                 <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Create Blog Post
-                                    </button>
+                                    <button type="submit" class="btn btn-primary">Create Blog Post</button>
                                 </div>
                             </div>
                         </form>
                     </div>
-
                 </div>
             </div>
         </div>
     </div>
+
+    <script type="text/javascript">
+        $('div.alert').delay(2000).slideUp(300);
+    </script>
 @endsection
