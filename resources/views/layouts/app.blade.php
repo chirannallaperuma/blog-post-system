a3ca29b727b54be7f30c84b7b15c5f363d4aeb52
chiran nallaperuma <chiran@intaap.com>
2024-09-23 16:26:16 +0530

    sweet alert added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,8 @@
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="{{ asset('plugins/sweetalert2/sweetalert2.all.min.js') }}"></script>
 </head>
 
 <body>
